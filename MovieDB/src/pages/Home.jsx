b5e756e5d6a2134a88ca7db75b5c1cc74f3d7c66
Mikaela <M.Norrelokke@gmail.com>
2021-09-10 22:51:18 +0200

    axios

--- MovieDB/src/pages/Home.jsx
+++ MovieDB/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import { useQuery } from 'react-query'
+import { getMovies } from '../services/API'
+
 
 const HomePage = () => {
+    const { data, status } = useQuery('movies', getMovies);
+    console.log(data);
 	return (
 		<Container className="py-3">
 			<h1>Welcome!</h1>
---
+++ MovieDB/src/services/API.js
@@ -0,0 +1,21 @@
+const baseURL = 'https://api.themoviedb.org'
+const APIKEY = '?api_key=89ff8f49db669e76da191e37de9c197b'
+
+
+
+const get = async (endpoint) => {
+    const res = await fetch(baseURL + endpoint + APIKEY)
+    if (!res.ok) {
+throw new Error("Something is wrong with the request!")
+    }
+    return res.json()
+}
+
+/**
+ * @returns Promise
+ */
+
+
+export const getMovies = async() => {
+    return get('/3/movie/550');
+}

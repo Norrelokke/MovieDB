9e6294c04e9df6641237663a2fc3cbcb5af96967
Mikaela <M.Norrelokke@gmail.com>
2021-09-10 13:57:51 +0200

    routes and pages

---
+++ MovieDB/src/components/Navigation.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+const Navigation = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="md">
+            <Container>
+                <Link to="/" className="navbar-brand">
+                    Movie api
+                </Link>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <NavLink to="/" className="nav-link">
+                            Home
+                        </NavLink>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navigation
---
+++ MovieDB/src/pages/Movie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+const Movie = () => {
+	return (
+		<Container className="py-3">
+			<h1>Movie</h1>
+		</Container>
+	)
+}
+
+export default Movie
